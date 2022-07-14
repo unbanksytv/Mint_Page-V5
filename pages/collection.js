606ce120df513a27a0a63a04260c2db574bb6d0e
UnbanksyTV <45092543+unbanksytv@users.noreply.github.com>
2022-07-14 02:32:08 +0200

    Add Collection Page

---
+++ pages/collection.js
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import tw from "tailwind-styled-components";
+
+// Components
+import Header from "../components/Header";
+import Collection from "../components/Collection";
+import Footer from "../components/Footer";
+import Collection from "../components/Collection";
+
+export default function Collection() {
+  return (
+    <Container>
+      <Head>
+        <title>The Photo Labs - Your NFT</title>
+        <meta name="description" content="Made with love by LiveTheLifeTV" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Collection />
+      <Footer />
+    </Container>
+  )
+}
+
+const Container = tw.div`
+ w-screen
+ h-screen
+ bg-black
+ text-white
+ px-7
+ flex
+ flex-col
+ justify-between
+`
